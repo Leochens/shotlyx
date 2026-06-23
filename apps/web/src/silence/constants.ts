f18ae6f91b431f904fafd6b3545a7e7637d3a1da
Leochens <18332518328@163.com>
2026-06-23 15:12:20 +0800

    调整去除静音默认参数

--- apps/web/src/silence/constants.ts
+++ apps/web/src/silence/constants.ts
@@ -1,8 +1,8 @@
 import type { SilenceDetectionOptions } from "./types";
 
 export const DEFAULT_SILENCE_DETECTION_OPTIONS: SilenceDetectionOptions = {
-	thresholdDb: -40,
-	minSilenceMs: 350,
+	thresholdDb: -50,
+	minSilenceMs: 500,
 	paddingMs: 100,
 	windowMs: 20,
 	mergeGapMs: 120,
